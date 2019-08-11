542446de890584cf515238366747f751f0a8c7be
yorifuji <yorifuji@gmail.com>
2019-08-11 23:29:21 +0900

    enable background task

--- skyway-callkit-sampler/AppDelegate.swift
+++ skyway-callkit-sampler/AppDelegate.swift
@@ -12,7 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var skywayAPIKey:String?
+    var skywayDomain:String?
 
+    var backgroundTaskID = UIBackgroundTaskIdentifier.invalid
+    var timer: Timer?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -27,10 +31,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        startBackgroundTask()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        stopBackgroundTask()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -44,3 +50,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate {
+    func startBackgroundTask() {
+        backgroundTaskID = UIApplication.shared.beginBackgroundTask(expirationHandler: nil)
+        timer = Timer.scheduledTimer(withTimeInterval: 30.0, repeats: false) { timer in
+            self.stopBackgroundTask()
+        }
+    }
+
+    func stopBackgroundTask() {
+        if backgroundTaskID != UIBackgroundTaskIdentifier.invalid {
+            UIApplication.shared.endBackgroundTask(backgroundTaskID)
+            backgroundTaskID = UIBackgroundTaskIdentifier.invalid
+        }
+        if timer != nil {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+}
